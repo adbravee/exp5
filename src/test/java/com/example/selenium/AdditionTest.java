04e9a92444be0ba847fa4f01d5d8ded7f0f08f65
Aditya <5023117@it..fcrit.ac.in>
2026-03-02 15:09:44 +0530

    Initial project

---
+++ src/test/java/com/example/selenium/AdditionTest.java
@@ -0,0 +1,46 @@
+package com.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AdditionTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        // Point to EdgeDriver location
+        System.setProperty("webdriver.edge.driver", "C:\\WebDriver\\msedgedriver.exe");
+        driver = new EdgeDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testAdditionWithEdge() {
+        driver.get("file:///C:/my-webapp/src/main/webapp/addition.html");
+
+        WebElement num1 = driver.findElement(By.id("num1"));
+        WebElement num2 = driver.findElement(By.id("num2"));
+        WebElement addBtn = driver.findElement(By.id("addBtn"));
+        WebElement result = driver.findElement(By.id("result"));
+
+        num1.sendKeys("10");
+        num2.sendKeys("20");
+        addBtn.click();
+
+        assertEquals("30", result.getText());
+    }
+}
